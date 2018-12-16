ab4552a0cd17a0581d3a381bedcbb7d41026d01a
Julio <jforero@ucenfotec.ac.cr>
2018-12-16 08:36:56 -0600

    Add interceptors to providers

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,  HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { UserModule } from './_modules/user/user.module'
 import { MaterialModule } from './_modules/material/material.module';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,10 @@ import { MaterialModule } from './_modules/material/material.module';
     UserModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
