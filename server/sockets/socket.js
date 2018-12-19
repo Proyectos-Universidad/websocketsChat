5d2167936d298c87c255442b29dbfacc3237eb38
Mary Paz <marayam1@ucenfotec.ac.cr>
2018-12-19 07:55:33 -0600

    Update chat.component

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -4,11 +4,9 @@ module.exports = (server) => {
   let io = socketIO(server);
 
   io.on('connection', (socket) => {
-    console.log('user connected');
 
     socket.on('new-message', (message) => {
       io.emit('new-message',message);
-      console.log(message);
     });
   
 
